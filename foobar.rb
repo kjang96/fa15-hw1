95b3102133a812babe11079a4d55c9964673a622
Kathy Jang <kathyjang@berkeley.edu>
2015-09-15 22:05:14 -0700

    hw1 update

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,12 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a) 
+  	a.map! {|x| x.to_i}
+  	a.map! {|x| x+=2}
+  	a = a.select{|x| x % 2 == 0}
+  	a = a.uniq
+  	a = a.select{|x| x <= 10}
+  	#a.inject{|sum, x| sum + x}
+  	return a.inject{|sum, x| sum + x}
+  end 
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,19 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  return (a+b) * (a+b) 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  a = a.sort
+  i = 0
+  while i < a.length do 
+  	a[i] += 1
+  	i += 1
+  end 
+  return a
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  return first_name + " " + last_name
 end
 
 def blockin_time(a)
